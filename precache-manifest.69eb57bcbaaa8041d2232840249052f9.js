c921426805c48fa34b5de471ee402e7a2350665e
Malina Randrianavony <designesse@malinas-imac.lan>
2018-12-19 10:05:08 -0700

    Updates

---
+++ precache-manifest.69eb57bcbaaa8041d2232840249052f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b1e7ffb457d81f91fb7",
+    "url": "/habits/static/css/main.40108ba9.chunk.css"
+  },
+  {
+    "revision": "7b1e7ffb457d81f91fb7",
+    "url": "/habits/static/js/main.7b1e7ffb.chunk.js"
+  },
+  {
+    "revision": "a93cec8a557ee382682a",
+    "url": "/habits/static/js/1.a93cec8a.chunk.js"
+  },
+  {
+    "revision": "682e102faff6bb99897e",
+    "url": "/habits/static/js/runtime~main.682e102f.js"
+  },
+  {
+    "revision": "a039d76730a8c78375d416a91bb8a708",
+    "url": "/habits/index.html"
+  }
+];
--- src/Constants.js
+++
@@ -1,7 +0,0 @@
-const Constants = {
-  HABITSTYPES : { 'daily': 0, 'weekly': 1},
-  WEEKDAYS: { 'Sunday': 0, 'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6 },
-  MONTHS: {'January': 0, 'February': 1, 'March': 2, 'April': 3, 'May': 4, 'June': 5, 'July': 6, 'August': 7, 'September': 8, 'October': 9, 'November': 10, 'December': 11 }
-};
-
-export default Constants;
